4c71c26dcb886573a6d37792f89e76ec1a4affbb
johanna-ohrnberg <j-ohrnberg@live.se>
2021-06-04 08:55:59 +0200

    update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import "../footer/footer.css";
 
 export default function Footer() {
     return (
-        <div>
-
-        </div>
+        <section className="footer">
+            <a href="tel:0703734320">0703 - 73 43 20</a>
+            <a href="mailto:j-ohrnberg@live.se">j-ohrnberg@live.se</a>
+        </section>
     )
 }
